38bf11bfacb5163ffa2ff811b3f2de556dc7b0da
Mani <mani.nz.mani@gmail.com>
2019-08-22 14:01:36 +1200

    my first commit

---
+++ src/main/java/nz/ac/eit/amazingtranslator/App.java
@@ -0,0 +1,19 @@
+package nz.ac.eit.amazingtranslator;
+
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "What is the number to translate" );
+        Scanner scan=new Scanner(System.in);
+        String numberToTranslateInString=scan.nextLine();
+        int numberToTranslate=Integer.parseInt(numberToTranslateInString);
+        System.out.println("The number to translate is:"+numberToTranslate);
+    }
+}
